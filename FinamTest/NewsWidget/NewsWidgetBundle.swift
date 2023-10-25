6ed45aaab0e209439ca88bc8ff9a4e292354eb35
Ярослав Куприянов <upakkomi@gmail.com>
2023-10-25 12:56:58 +0300

    widget configuring started

---
+++ FinamTest/NewsWidget/NewsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetBundle.swift
+//  NewsWidget
+//
+//  Created by Ярослав Куприянов on 25.10.2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidget()
+    }
+}
