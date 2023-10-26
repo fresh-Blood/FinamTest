c6ef1bded327d6dd2c95e77d4efdd5d7ec6d81d2
Ярослав Куприянов <upakkomi@gmail.com>
2023-10-26 16:45:31 +0300

    notification added

--- News/App/Helpers/AppDelegate.swift
+++ News/App/Helpers/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         SoundManager.shared.player.prepareToPlay()
         try? AVAudioSession.sharedInstance().setCategory(.playback, options: .mixWithOthers)
+        configureNotifications()
         return true
     }
     
@@ -17,5 +18,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationWillTerminate(_ application: UIApplication) {
         SoundManager.shared.player.stop()
+    }
+}
+
+extension AppDelegate {
+    private func configureNotifications() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        notificationCenter.requestAuthorization(options:  [.alert, .badge, .carPlay, .providesAppNotificationSettings, .sound]) { granted, error in
+            guard error == nil, granted else { return }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "News"
+            content.body = "More topics arrived"
+            content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "notification.mp3"))
+            
+            var dateComponents = DateComponents()
+            dateComponents.weekday = 6
+            dateComponents.hour = 17
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            
+            let uuid = UUID().uuidString
+            let request = UNNotificationRequest(identifier: uuid,
+                                                content: content,
+                                                trigger: trigger)
+            
+            notificationCenter.add(request)
+        }
     }
 }
